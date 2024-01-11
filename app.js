a1854bc6155e53293bc2f5e47d5a5977c994cb42
Sergio Fenoll <sergio@fenoll.be>
2024-01-11 12:00:31 +0100

    feat: store and link converted pdf

--- app.js
+++ app.js
@@ -1,43 +1,8 @@
-import { app, query, update, errorHandler, sparqlEscapeString } from 'mu';
+import { app, errorHandler } from 'mu';
 import fs from 'fs';
 import { StreamUpload, LargeFileUploadTask } from "@microsoft/microsoft-graph-client";
-import { USER_ID, MS_GRAPH_CLIENT as client } from './config';
-
-const parseSparqlResults = (data) => {
-  if (!data) return;
-  const vars = data.head.vars;
-  return data.results.bindings.map((binding) => {
-    const obj = {};
-    vars.forEach((varKey) => {
-      if (binding[varKey]) {
-        obj[varKey] = binding[varKey].value;
-      }
-    });
-    return obj;
-  });
-};
-
-const getFileById = async function (fileId) {
-  const q = `
-PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
-PREFIX dbpedia: <http://dbpedia.org/ontology/>
-PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
-SELECT DISTINCT (?virtualFile AS ?uri) (?physicalFile AS ?physicalUri) (?uuid as ?id) ?name ?extension
-WHERE {
-    ?virtualFile a nfo:FileDataObject ;
-        mu:uuid ${sparqlEscapeString(fileId)} ;
-        mu:uuid ?uuid .
-    ?physicalFile a nfo:FileDataObject ;
-        nie:dataSource ?virtualFile .
-    ?virtualFile nfo:fileName ?name .
-    ?virtualFile dbpedia:fileExtension ?extension .
-}
-  `;
-  const result = await query(q);
-  const [file] = parseSparqlResults(result);
-  return file;
-};
+import { FILE_JSONAPI_TYPE, USER_ID, MS_GRAPH_CLIENT as client } from './cfg';
+import { getFile, setFileSource, storeFile } from './lib/file';
 
 
 app.get('/', function(_req, res) {
@@ -45,52 +10,13 @@ app.get('/', function(_req, res) {
 });
 
 app.post('/files/:id/convert', async (req, res) => {
-  // const upn = await client
-  //       .api("/users")
-  //       .filter(`mail eq 'sergio.fenoll@6r3p7j.onmicrosoft.com'`)
-  //       .select("userPrincipalName")
-  //       .get();
-  // console.debug(upn)
-
   const drives = await client
         .api(`/users/${USER_ID}/drives`)
         .get();
-  console.debug(drives);
   const fileId = req.params.id;
-  const file = await getFileById(fileId);
+  const file = await getFile(fileId);
   file.path = file.physicalUri.replace('share://', '/share/');
 
-  // const options = {
-  //   // Relative path from root to destination folder
-  //   path: './',
-  //   // file is a File object, typically from an <input type="file"/>
-  //   fileName: file.name,
-  //   rangeSize: 1024 * 1024,
-  //   uploadEventHandlers: {
-  //     // Called as each "slice" of the file is uploaded
-  //     progress: (range, e) => {
-  //       console.log(`Uploaded ${range?.minValue} to ${range?.maxValue}`);
-  //     }
-  //   }
-  // };
-
-  // try {
-  //   const stats = fs.statSync(file.path);
-  //   const totalSize = stats.size;
-  //   const readStream = fs.createReadStream(file.path);
-  //   const fileObject = new StreamUpload(readStream, file.name, totalSize);
-  //   // Create a OneDrive upload task
-  //   const uploadTask = await OneDriveLargeFileUploadTask
-  //     .createTaskWithFileObject(client, fileObject, options);
-
-  //   // Do the upload
-  //   const uploadResult = await uploadTask.upload();
-
-  //   // The response body will be of the corresponding type of the
-  //   // item being uploaded. For OneDrive, this is a DriveItem
-  //   const driveItem = uploadResult.responseBody;
-  //   console.log(`Uploaded file with ID: ${driveItem.id}`);
-  //   console.debug(driveItem);
   try {
     const uploadSessionPayload = {
       item: {
@@ -109,22 +35,38 @@ app.post('/files/:id/convert', async (req, res) => {
     const options = {
       rangeSize: 327680,
       uploadEventHandlers: {
-        progress: (range) => { console.info(`Progress uploading file <${file.uri}>. Bytes: [${range.minValue}, ${range.maxValue}]`) }
+        progress: (range) => {
+          console.info(
+            `Progress uploading file <${file.uri}>. Bytes: [${range.minValue}, ${range.maxValue}]`
+          )
+        }
       },
     };
     const uploadTask = new LargeFileUploadTask(client, fileObject, uploadSession, options);
     const result = await uploadTask.upload();
-    console.debug(result);
   } catch (err) {
     console.log(`Error uploading file: ${JSON.stringify(err)}`);
-    console.debug(err);
+    console.error(err);
   }
 
-  const downloadStream = await client
+  const buffer = await client
         .api(`/users/${USER_ID}/drive/root:/${file.id}.${file.extension}:/content?format=pdf`)
         .get();
-  const writeStream = fs.createWriteStream('/share/out.pdf');
-  downloadStream.pipe(writeStream);
+
+  const newFile = await storeFile(`${file.id}.pdf`, buffer);
+  await setFileSource(file.uri, newFile.uri);
+
+  return res
+    .status(200)
+    .send({
+      data: [{
+        type: FILE_JSONAPI_TYPE,
+        id: newFile.id,
+        attributes: {
+          uri: newFile.uri,
+        }
+      }]
+    });
 });
 
 app.use(errorHandler);
---
+++ types.js
@@ -0,0 +1,22 @@
+/**
+ * @typedef {Object} PhysicalFile
+ * @property {string} uri
+ * @property {string} id
+ * @property {string} name
+ * @property {string} format
+ * @property {number} size
+ * @property {string} extension
+ * @property {Date} created
+ */
+
+/**
+ * @typedef {Object} VirtualFile
+ * @property {string} uri
+ * @property {string} id
+ * @property {string} name
+ * @property {string} format
+ * @property {number} size
+ * @property {string} extension
+ * @property {Date} created
+ * @property {PhysicalFile} physicalFile
+ */
